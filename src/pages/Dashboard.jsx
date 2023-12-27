c3841cab636d462d4638c4b72a810b6474a4ceda
ImTrikk <128696071+Chan-cannot-code@users.noreply.github.com>
2023-12-27 19:20:57 +0800

    update dependencies and added api folder

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+export const Dashboard = () => {
+	return (
+		<>
+			<div>
+				<h1>Admin Dashboard</h1>
+			</div>
+		</>
+	);
+};
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,11 @@
+import { Login } from "./Login";
+
+export const Landing = () => {
+	return (
+		<>
+			<div className="lg:mx-auto mx-56 max-w-7xl">
+				<Login />
+			</div>
+		</>
+	);
+};
