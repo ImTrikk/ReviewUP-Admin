d566bb1f443df20fa6baaac5e53764fd47566f80
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2024-02-22 23:31:22 +0800

    feat: initiallie otp and forgot password

--- api/Controllers/AuthController.js
+++ api/Controllers/AuthController.js
@@ -71,10 +71,9 @@ export const Logout = async (req, res) => {
 //validate email first with otp
 export const ForgotPassword = () => {
 	// endpoint will accept {email and as well as new password}
-
 	try {
 		return res.status(200).json({ message: "Success change password" });
 	} catch (err) {
-		console.log(err);
+		return res.status(500).json({message: "Internal Server Error"})
 	}
 };
--- api/Middleware/OneTimePassword.js
+++ api/Middleware/OneTimePassword.js
@@ -1,3 +1,20 @@
-export const OneTimePassword = async (req, res, next) => {
-	// Your
+export const SendOtp = async (req, res) => {
+	try {
+		// implemnt logic for recieving the email and chekcing in db
+		// send otp to the requesor
+		return req.status(200).json({message: "One Time Password to {email}"})
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ message: "Internal Server Error" });
+	}
+};
+
+//checking otp sent by user
+export const CheckOtp = (req, res, next) => {
+	try {
+		return next();
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ message: "Internal Server Error" });
+	}
 };
